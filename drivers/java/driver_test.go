68ac10289bc846775cf4eb00ca5c6d7261c81ed7
Mahmood Ali <mahmood@hashicorp.com>
2018-11-07 18:00:34 -0800

    Fix java driver tests

--- drivers/java/driver_test.go
+++ drivers/java/driver_test.go
@@ -1,7 +1,6 @@
 package java
 
 import (
-	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
@@ -73,7 +72,7 @@ func TestDriver_Jar_Start_Wait(t *testing.T) {
 	cleanup := harness.MkAllocDir(task, true)
 	defer cleanup()
 
-	copyFile("./test-resources/java/demoapp.jar", filepath.Join(task.TaskDir().Dir, "demoapp.jar"), t)
+	copyFile("./test-resources/demoapp.jar", filepath.Join(task.TaskDir().Dir, "demoapp.jar"), t)
 
 	handle, _, err := harness.StartTask(task)
 	require.NoError(err)
@@ -112,7 +111,7 @@ func TestDriver_Jar_Stop_Wait(t *testing.T) {
 	cleanup := harness.MkAllocDir(task, true)
 	defer cleanup()
 
-	copyFile("./test-resources/java/demoapp.jar", filepath.Join(task.TaskDir().Dir, "demoapp.jar"), t)
+	copyFile("./test-resources/demoapp.jar", filepath.Join(task.TaskDir().Dir, "demoapp.jar"), t)
 
 	handle, _, err := harness.StartTask(task)
 	require.NoError(err)
@@ -246,6 +245,8 @@ func TestJavaCmdArgs(t *testing.T) {
 }
 
 func basicTask(t *testing.T, name string, taskConfig map[string]interface{}) *drivers.TaskConfig {
+	t.Helper()
+
 	task := &drivers.TaskConfig{
 		ID:   uuid.Generate(),
 		Name: name,
@@ -266,16 +267,15 @@ func basicTask(t *testing.T, name string, taskConfig map[string]interface{}) *dr
 }
 
 func encodeDriverHelper(t *testing.T, task *drivers.TaskConfig, taskConfig map[string]interface{}) {
+	t.Helper()
+
 	evalCtx := &hcl.EvalContext{
 		Functions: shared.GetStdlibFuncs(),
 	}
 	spec, diag := hclspec.Convert(taskConfigSpec)
 	require.False(t, diag.HasErrors())
 	taskConfigCtyVal, diag := shared.ParseHclInterface(taskConfig, spec, evalCtx)
-	if diag.HasErrors() {
-		fmt.Println("conversion error", diag.Error())
-	}
-	require.False(t, diag.HasErrors())
+	require.Empty(t, diag.Errs())
 	err := task.EncodeDriverConfig(taskConfigCtyVal)
 	require.Nil(t, err)
 
